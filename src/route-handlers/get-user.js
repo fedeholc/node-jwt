8e7812da68b8bc2eba99784dd70719785da63cda
Fede <federicoholc@gmail.com>
2024-09-25 18:26:04 -0300

    wip ok

--- src/route-handlers/get-user.js
+++ src/route-handlers/get-user.js
@@ -1,10 +1,4 @@
-import { jwtVerify } from "jose";
-import { secretKey } from "../global-store.js";
-
-//TODO: esta tendría que llamarse algo así como handleGetUser y solo validar el token para devolver al front.
-//TODO: Por lo tanto no checkiar acá con la base de datos.
 //TODO: el checkeo con la base de datos tendría que hacerse al generar el refresh token.
-//TODO: ahora que lo pienso acá podría pasar también por el middleware de verificar el token y que me llegue en el req.
 
 export async function handleGetUser(req, res) {
   if (req.payload) {
